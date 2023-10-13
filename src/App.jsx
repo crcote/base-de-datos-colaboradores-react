69113f2ad1a4bdcad55af16de00d5d300df0cdfd
Fel-Jim <jimenez.edu.cruz@gmail.com>
2023-10-13 02:17:32 -0300

    se agrega Listado

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
-import { useState } from 'react'
-import Formulario from './components/Formulario'
-import Listado from './components/Listado'
-import Alert from './components/Alert'
-import { colaboradoresIniciales} from './components/colaboradores'
+import { useState } from "react";
+import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
+import Alert from "./components/Alert";
+import { colaboradoresIniciales } from "./components/colaboradores";
 
 function App() {
-  const [errorForm, setErrorForm] = useState()
-  const [errorMail, setErrorMail] = useState ()
-  const [colaboradores, setColaboradores] = useState(colaboradoresIniciales)
+  const [errorForm, setErrorForm] = useState();
+  const [errorMail, setErrorMail] = useState();
+  const [colaboradores, setColaboradores] = useState(colaboradoresIniciales);
 
   return (
     <>
-    <Formulario
-    errorForm ={errorForm}
-    setErrorForm={setErrorForm}
-    errorMail={errorMail}
-    setErrorMail={setErrorMail}
-    colaboradores={colaboradores}
-    setColaboradores={setColaboradores}
-    />
-    {errorForm ? (
-      <Alert message="Completa todos los campos" color="danger" />
-    ) : null}
-    {errorMail && !errorForm ? (
-      <Alert message="Formato mail incorrecto" color="danger" />
-    ) : null}
-    {errorForm == false && errorMail == false ? (
-      <Alert message="Registro Exitoso" color="success" />
-    ) : null}
-    <Listado colaboradores={colaboradores}/>
+      <Formulario
+        errorForm={errorForm}
+        setErrorForm={setErrorForm}
+        errorMail={errorMail}
+        setErrorMail={setErrorMail}
+        colaboradores={colaboradores}
+        setColaboradores={setColaboradores}
+      />
+      {errorForm ? (
+        <Alert message="Completa todos los campos" color="danger" />
+      ) : null}
+      {errorMail && !errorForm ? (
+        <Alert message="Formato mail incorrecto" color="danger" />
+      ) : null}
+      {errorForm == false && errorMail == false ? (
+        <Alert message="Registro Exitoso" color="success" />
+      ) : null}
+      <Listado colaboradores={colaboradores} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const Listado = ({colaboradores}) => {
-return (
+const Listado = ({ colaboradores }) => {
+  return (
     <>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Nombre Colaborador</th>
+              <th scope="col">E-mail</th>
+              <th scope="col">Edad</th>
+              <th scope="col">Cargo</th>
+              <th scope="col">Teléfono</th>
+            </tr>
+          </thead>
+          <tbody>
+            {colaboradores.map((e) => (
+              <tr>
+                <td>{e.nombre}</td>
+                <td>{e.correo}</td>
+                <td>{e.edad}</td>
+                <td>{e.cargo}</td>
+                <td>{e.telefono}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-    )
-}
+  );
+};
 
-export default Listado
+export default Listado;
